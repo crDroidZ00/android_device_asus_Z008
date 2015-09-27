3485dfb66966a9abd12e814d7356a09e73af5b4f
Călin Neamţu <nilac8991@gmail.com>
2015-09-27 11:36:40 +0200

    crDroid Re-Style
    Signed-off-by: Călin Neamţu <nilac8991@gmail.com>

--- crdroid.mk
+++ crdroid.mk
@@ -2,10 +2,10 @@
 TARGET_SCREEN_HEIGHT := 720
 TARGET_SCREEN_WIDTH := 1280
 
-# Inherit some common CM stuff.
+# Inherit some common crDroid stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z008/device.mk)
@@ -15,7 +15,7 @@ DEVICE_PACKAGE_OVERLAYS += device/asus/Z008/overlay
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_Z008
+PRODUCT_NAME := crdroid_Z008
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z008
 PRODUCT_MANUFACTURER := asus
